8cd5ad1a8d41d8d73366bb2406f5ca5f0a9e6301
Ryan Yogan <ryanyogan@gmail.com>
2024-11-13 13:10:00 -0600

    toggle topic on click

--- lib/slax_web/live/chat_room_live.ex
+++ lib/slax_web/live/chat_room_live.ex
@@ -11,8 +11,12 @@ defmodule SlaxWeb.ChatRoomLive do
           <h1 class="text-sm font-bold leading-none">
             #<%= @room.name %>
           </h1>
-          <div class="text-xs leading-none h-3.5">
-            <%= @room.topic %>
+          <div class="text-xs leading-none h-3.5" phx-click="toggle-topic">
+            <%= if @hide_topic? do %>
+              <span class="text-slate-600">[Topic Hidden]</span>
+            <% else %>
+              <%= @room.topic %>
+            <% end %>
           </div>
         </div>
       </div>
@@ -23,6 +27,10 @@ defmodule SlaxWeb.ChatRoomLive do
   def mount(_params, _session, socket) do
     room = Room |> Repo.all() |> List.first()
 
-    {:ok, assign(socket, :room, room)}
+    {:ok, assign(socket, hide_topic?: false, room: room)}
+  end
+
+  def handle_event("toggle-topic", _unsigned_params, socket) do
+    {:noreply, assign(socket, hide_topic?: !socket.assigns.hide_topic?)}
   end
 end
