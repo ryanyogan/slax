21b8a871e5a3406b793c679d42cf9ffbfae4b253
Ryan Yogan <ryanyogan@gmail.com>
2024-11-13 18:02:22 -0600

    show message username

--- lib/slax_web/live/chat_room_live.ex
+++ lib/slax_web/live/chat_room_live.ex
@@ -46,7 +46,7 @@ defmodule SlaxWeb.ChatRoomLive do
         <ul class="relative z-10 flex items-center gap-4 px-4 sm:px-6 lg:px-8 justify-end">
           <%= if @current_user do %>
             <li class="text-[0.8125rem] leading-6 text-zinc-900">
-              <%= @current_user.email %>
+              <%= username(@current_user) %>
             </li>
             <li>
               <.link
@@ -121,13 +121,17 @@ defmodule SlaxWeb.ChatRoomLive do
       <div class="ml-2">
         <div class="-mt-1">
           <.link class="text-sm font-semibold hover:underline">
-            <span>User</span>
+            <span><%= username(@message.user) %></span>
           </.link>
           <p class="text-sm"><%= @message.body %></p>
         </div>
       </div>
     </div>
     """
+  end
+
+  defp username(user) do
+    user.email |> String.split("@") |> List.first() |> String.capitalize()
   end
 
   @impl true
