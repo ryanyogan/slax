e7083097648b97746db86242eccf80b30a8339ca
Ryan Yogan <ryanyogan@gmail.com>
2024-11-16 12:15:06 -0600

    extract RoomComponents

--- lib/slax_web/components/chat_components.ex
+++ lib/slax_web/components/chat_components.ex
@@ -6,6 +6,20 @@ defmodule SlaxWeb.ChatComponents do
   alias Slax.Chat.{Message, Room}
   alias Slax.Accounts.User
 
+  attr :form, Phoenix.HTML.Form, required: true
+
+  def room_form(assigns) do
+    ~H"""
+    <.simple_form for={@form} id="room-form" phx-change="validate-room" phx-submit="save-room">
+      <.input field={@form[:name]} type="text" label="Name" />
+      <.input field={@form[:topic]} type="text" label="Topic" />
+      <:actions>
+        <.button phx-disable-with="Saving..." class="w-full">Save</.button>
+      </:actions>
+    </.simple_form>
+    """
+  end
+
   attr :message, Message, required: true
   attr :dom_id, :string, required: true
   attr :timezone, :string, required: true
--- lib/slax_web/live/chat_room_live/edit.ex
+++ lib/slax_web/live/chat_room_live/edit.ex
@@ -3,6 +3,8 @@ defmodule SlaxWeb.ChatRoomLive.Edit do
 
   alias Slax.Chat
 
+  import SlaxWeb.ChatComponents
+
   def render(assigns) do
     ~H"""
     <div class="mx-auto w-96 mt-12">
@@ -18,14 +20,7 @@ defmodule SlaxWeb.ChatRoomLive.Edit do
         </:actions>
       </.header>
 
-      <.simple_form for={@form} id="room-form" phx-change="validate-room" phx-submit="save-room">
-        <.input field={@form[:name]} type="text" label="Name" phx-debounce />
-        <.input field={@form[:topic]} type="text" label="Topic" phx-debounce />
-
-        <:actions>
-          <.button phx-disable-with="Saving..." class="w-full">Save</.button>
-        </:actions>
-      </.simple_form>
+      <.room_form form={@form} />
     </div>
     """
   end
